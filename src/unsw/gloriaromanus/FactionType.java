261381cc3a3901d2d031e5c2abd6b7f4f4cc413c
Ezra <other.ezhui@gmail.com>
2020-10-23 15:36:58 +1100

    can get factiontype enum from faction name now

--- src/unsw/gloriaromanus/FactionType.java
+++ src/unsw/gloriaromanus/FactionType.java
@@ -1,22 +1,40 @@
 package unsw.gloriaromanus;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
+
 public enum FactionType {
 	// Placeholders
-	ROME		("Rome", "Roman", "Romans"),
-	GAUL		("Gaul", "Gallic", "Gauls");
+	ROME		("Roman", "Romans"),
+	GAUL		("Gallic", "Gauls");
 	
-	private String name;
+	private String title;
 	private String adjective;
 	private String plural;
 
-	private FactionType(String name, String adjective, String plural) {
-		this.name = name;
+	public static FactionType fromName(String name) {
+		name = name.toLowerCase();
+		for (FactionType t : FactionType.values()) {
+			if(Objects.equals(t.name().toLowerCase(), name)) {
+				return t;
+			}
+		}
+		return null;
+	}
+	
+	// Title is determined by the name() function which gives the enum name as it appears in code.
+	private FactionType(String adjective, String plural) {
+		// Extract Title name
+		this.title = Character.toUpperCase(this.name().charAt(0)) +
+				this.name().substring(1).toLowerCase();
+		
 		this.adjective = adjective;
 		this.plural = plural;
 	}
 
-	public String getName() {
-		return name;
+	public String getTitle() {
+		return title;
 	}
 
 	public String getAdjective() {
