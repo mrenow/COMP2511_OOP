2902b090ba11fe615f88331f2f091b13d036e995
Ezra <other.ezhui@gmail.com>
2020-10-23 16:28:18 +1100

    Changed faction interface

--- src/unsw/gloriaromanus/Faction.java
+++ src/unsw/gloriaromanus/Faction.java
@@ -3,12 +3,19 @@ package unsw.gloriaromanus;
 import java.util.List;
 
 public class Faction {
-	public String getName() {return null;}
+	private FactionType type;
+	
+	public FactionType getType() {return type;}
+	public String getTitle() {return type.getTitle();}
 	
 	public int getTreasury() {return 0;}
 	
 	public int getTotalWealth() {return 0;}
 	
 	public List<Province> getProvinces(){return null;}
+	
+	public Province getProvince(String name) {
+		return null;
+	}
 	
 }
