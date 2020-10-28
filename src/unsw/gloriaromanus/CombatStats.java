e33b686393baad3320e76c24975a2bcee6b09707
Ezra <other.ezhui@gmail.com>
2020-10-29 05:17:34 +1100

    as before

--- src/unsw/gloriaromanus/CombatStats.java
+++ src/unsw/gloriaromanus/CombatStats.java
@@ -19,8 +19,6 @@ public class CombatStats {
 	// base = Double.NEGATIVE_INFINITY -> exactly 0 of that characteristic, regardless of modifiers.
 	// Otherwise, the characteristic is calculated using armourAdd and armourMult.
 	private double armourBase = 0; 
-	private double moraleBase = 0;
-	private double speedBase = 0; 
 	private double attackBase = 0; 
 	
 
@@ -29,23 +27,19 @@ public class CombatStats {
 
 	// Should not change 
 	private double armourMult = 1;
-	private double speedMult = 1;
 	private double attackMult = 1;
 	
 	private double armourAdd = 0;
-	private double speedAdd = 0;
 	private double attackAdd = 0;  
 
 
 	@JsonCreator
 	private CombatStats() {}
 	
-	public CombatStats(double armour, double morale, double speed, double attack, double defenseSkill,
+	public CombatStats(double armour, double attack, double defenseSkill,
 			double shieldDefense) {
 		super();
 		this.armourBase = armour;
-		this.moraleBase = morale;
-		this.speedBase = speed;
 		this.attackBase = attack;
 		this.defenseSkill = defenseSkill;
 		this.shieldDefense = shieldDefense;
@@ -62,15 +56,6 @@ public class CombatStats {
 			return armourBase;
 		}
 		return Math.max(1, (armourBase + armourAdd) * armourMult);
-	}
-	public double getSpeed() {
-		if (speedBase == Double.NEGATIVE_INFINITY) {
-			return 0;
-		}
-		if (speedBase == Double.POSITIVE_INFINITY) {
-			return speedBase;
-		}
-		return Math.max(1, (speedBase + speedAdd) * speedMult);
 	}
 	public double getAttack() {
 		if (attackBase == Double.NEGATIVE_INFINITY) {
@@ -101,18 +86,6 @@ public class CombatStats {
 		}
 		this.armourBase = armour;
 	}
-	public void setMorale(double morale) {
-		if (moraleBase == Double.NEGATIVE_INFINITY) {
-			return;
-		}
-		this.moraleBase = morale;
-	}
-	public void setSpeed(double speed) {
-		if (speedBase == Double.NEGATIVE_INFINITY) {
-			return;
-		}
-		this.speedBase = speed;
-	}
 	public void setAttack(double attack) {
 		if (attackBase == Double.NEGATIVE_INFINITY) {
 			return;
@@ -135,9 +108,6 @@ public class CombatStats {
 	void addArmour(double armour) {
 		this.armourAdd += armour;
 	}
-	void addSpeed(double speed) {
-		this.speedAdd += speed;
-	}
 	void addAttack(double attack) {
 		this.attackAdd += attack;
 	}
@@ -153,9 +123,6 @@ public class CombatStats {
 		this.armourMult *= armourMult;
 	}
 
-	void multSpeed(double speedMult) {
-		this.speedMult *= speedMult;
-	}
 	void multAttack(double attackMult) {
 		this.attackMult *= attackMult;
 	}
--- src/unsw/gloriaromanus/MoraleData.java
+++ src/unsw/gloriaromanus/MoraleData.java
@@ -9,55 +9,64 @@ import java.util.List;
 public class MoraleData {
 	public EnumMap<BattleSide, SideData> data = new EnumMap<>(BattleSide.class);
 	
+	private class SideData {
+		private List<Unit> army;
+		private Unit unit;
+		private double moraleBase;
+		private double moraleAdd = 0;
+		private double moraleMult = 1;
+		SideData(Unit unit, List<Unit> army){
+			this.unit = unit;
+			this.army = army;
+			this.moraleBase = unit.getMorale();
+		}
+	}
+	
 	public MoraleData(Unit attackerUnit, Unit defenderUnit, List<Unit> attackerArmy, List<Unit> defenderArmy) {
 		data.put(ATTACKER, new SideData(attackerUnit, attackerArmy));
 		data.put(ATTACKER, new SideData(defenderUnit, defenderArmy));
 		
 	}
 	
+	
 	public List<Unit> getArmy(BattleSide side){
-		return get(side).army;
-	}
-	public Unit getUnit(BattleSide side){
-		return get(side).unit;
+		return data.get(side).army;
 	}
 	
+	public Unit getUnit(BattleSide side){
+		return data.get(side).unit;
+	}
+	
+	/**
+	 * This should be called only once with non-infinite values for val.
+	 * 
+	 */
 	public void setMorale(BattleSide side, double val) {
-		if(get(side).morale != Double.NEGATIVE_INFINITY) {
-			get(side).morale = val;
+		// Neg inf proceeds all
+		if(data.get(side).moraleBase != Double.NEGATIVE_INFINITY) {
+			data.get(side).moraleBase = val;
 		}
 	}
 	
 	public void addMorale(BattleSide side, double val) {
-		get(side).moraleAdd += val;
+		data.get(side).moraleAdd += val;
 	}
 	
 	public void multMorale(BattleSide side, double val) {
-		get(side).moraleMult *= val;
+		data.get(side).moraleMult *= val;
 	}
-	
+	/**
+	 * 
+	 * @param side
+	 * @return The final morale calculation for the unit of the given side.
+	 */
 	public double getMorale(BattleSide side) {
-		if(get(side).morale == Double.NEGATIVE_INFINITY) {
+		if(data.get(side).moraleBase == Double.NEGATIVE_INFINITY) {
 			return 0;
+		}else if (data.get(side).moraleBase == Double.POSITIVE_INFINITY) {
+			return data.get(side).moraleBase;
 		}
-		return (get(side).morale + get(side).moraleAdd) * get(side).moraleMult;
-	}
-	
-	private SideData get(BattleSide side) {
-		return data.get(side);
-	}
-	
-	private class SideData {
-		private List<Unit> army;
-		private Unit unit;
-		private double morale;
-		private double moraleAdd = 0;
-		private double moraleMult = 1;
-		SideData(Unit unit, List<Unit> army){
-			this.unit = unit;
-			this.army = army;
-			this.morale = unit.getMorale();
-		}
+		return (data.get(side).moraleBase + data.get(side).moraleAdd) * data.get(side).moraleMult;
 	}
 }
 
