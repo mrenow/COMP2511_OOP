ac25fa10ed374358737af0d1df1a86c4c9bd6d11
Ezra <other.ezhui@gmail.com>
2020-11-02 05:16:36 +1100

    victory conditions (might) be sereializable

--- src/unsw/gloriaromanus/VicCondition/VicLeaf.java
+++ src/unsw/gloriaromanus/VicCondition/VicLeaf.java
@@ -1,7 +1,10 @@
 package unsw.gloriaromanus.VicCondition;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
+@JsonAutoDetect(fieldVisibility = Visibility.ANY, creatorVisibility = Visibility.ANY)
 public class VicLeaf implements VicComponent{
     private String goal="UNSETED";
 
