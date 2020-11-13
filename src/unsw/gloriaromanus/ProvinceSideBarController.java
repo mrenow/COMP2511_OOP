5dd44fe469547e6aa058022d782ec2307549f509
z5090449 <d.tai@student.unsw.edu.au>
2020-11-13 19:10:18 +0800

    Handling of province selection,move,invade implemented

--- src/unsw/gloriaromanus/ProvinceSideBarController.java
+++ src/unsw/gloriaromanus/ProvinceSideBarController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import unsw.engine.*;
@@ -36,11 +37,14 @@ public class ProvinceSideBarController extends Controller implements Observer<Pr
     @FXML private Label provinceUnitLabel;
     @FXML private Button trainBtn;
     @FXML private Button moveBtn;
-    @FXML private Button selectTarget;
+    @FXML private Button selectActionProvince;
+    @FXML private Button selectTargetProvince;
     @FXML private TextField trainTextField;
-    @FXML private ToggleButton toggleSelect;
-    @FXML private TextField invading_province;
-    @FXML private TextField opponent_province;
+    @FXML private TextField selected_province;
+    @FXML private TextField action_province;
+    @FXML private TextField target_province;
+    @FXML private TextArea selectedProvinceUnitsList;
+    @FXML private TextArea unitsInTraining;
 
     private String selectedTargetthing;
 
@@ -66,26 +70,29 @@ public class ProvinceSideBarController extends Controller implements Observer<Pr
     @FXML
     public void handleMove(ActionEvent e) {
         String u = provinceUnitCB.getValue().toString();
+        // If "All Units" tag chosen
         if (u == "All Units") {
-            // Handle move or attack
-            if (initialProvince.getOwner().equals(province.getOwner())) {
+            // If target province is player's own province
+            if (targetProvince.getOwner().equals(initialProvince.getOwner())) {
                 // Call move method
-                game.move(initialProvince.getUnits(), province);
+                game.move(initialProvince.getUnits(), targetProvince);
             }
+            // Else enemy province so invade
             else {
                 // Call invade method
                 game.invade(initialProvince, targetProvince);
             }
         }
+        // Else any other individual unit tag chosen
         else {
             for (Unit unit : initialProvince.getUnits()) {
                 if (u == unit.getName()) {
                     unitList = new ArrayList<Unit>();
                     unitList.add(unit);
                     // Handle move or attack
-                    if (initialProvince.getOwner().equals(province.getOwner())) {
+                    if (targetProvince.getOwner().equals(initialProvince.getOwner())) {
                         // Call move method
-                        game.move(unitList, province);
+                        game.move(unitList, targetProvince);
                     }
                     else {
                         // Call invade method
@@ -94,14 +101,38 @@ public class ProvinceSideBarController extends Controller implements Observer<Pr
                 }
             }
         }
-        
     }
 
-    // When this button is selected, choose a 2nd province and store in list(?)
+    // Displays the name of the selected action province along with its information
+    @FXML
+    public void handleSelectProvince() {
+        provinceUnitCB.getItems().clear();
+        if (!province.getOwner().equals(game.getCurrentTurn())) {
+            System.out.println("Action province must be your own province.");
+        }
+        else {
+            this.initialProvince = province;
+            action_province.setText(initialProvince.getName());
+
+            provinceUnitCB.getItems().add("All Units");
+            for (Unit u : initialProvince.getUnits()) {
+                provinceUnitCB.getItems().add(u.getName());
+            }
+            System.out.println("Action province selected.");
+        }
+    }
     @FXML
     public void handleSelectTarget() {
-        //invading_province.setText(myProvince);
-        this.initialProvince = province;
+        this.targetProvince = province;
+        target_province.setText(targetProvince.getName());
+
+        if (targetProvince.getOwner().equals(game.getCurrentTurn())) {
+            moveBtn.setText("Move");
+        }
+        else {
+            moveBtn.setText("Invade");
+        }
+        System.out.println("Selected province is: " + targetProvince.getName());
     }
 
     //@Override
@@ -118,25 +149,31 @@ public class ProvinceSideBarController extends Controller implements Observer<Pr
 
         // These items are for configuring the choicebox for units in province
         provinceUnitLabel.setText("");
-        provinceUnitCB.getItems().add("All Units");
-        provinceUnitCB.getItems().add("Unit1");
-        provinceUnitCB.getItems().add("Unit2");
-        provinceUnitCB.getItems().add("Unit3");
+        //provinceUnitCB.getItems().add("All Units");
+        //provinceUnitCB.getItems().add("Unit1");
+        //provinceUnitCB.getItems().add("Unit2");
+        //provinceUnitCB.getItems().add("Unit3");
     }
 
     @Override
     public void update(ProvinceFeatureInfo p) {
-        //this.province = p.getProvince();
+        System.out.println("Selected province is: " + p.getName());
+        System.out.println("Selected province owner is: " + p.getOwner().getTitle());
+        selected_province.setText(p.getName());
+        this.province = p.getProvince();
+        this.myProvince = p.getName();
+        /*
         if (p.getOwner().equals(game.getCurrentTurn())) {
+            System.out.println("This province belongs to me.");
             this.province = p.getProvince();
             this.myProvince = p.getName();
-            moveBtn.setText("Move");
         }
         else {
+            System.out.println("This province is either enemy or NO_ONE");
             this.targetProvince = p.getProvince();
             this.enemyProvince = p.getName();
-            moveBtn.setText("Invade");
         }
+        */
     }
 
 /*
