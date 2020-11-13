62aac91557d263f30f139e3cccf3f5c044efff6b
z5090449 <d.tai@student.unsw.edu.au>
2020-11-13 21:47:50 +0800

    deleted useless files

--- src/unsw/gloriaromanus/P_SelectionSubject.java
+++
@@ -1,9 +0,0 @@
-package unsw.gloriaromanus;
-
-public interface P_SelectionSubject {
-    
-    public void register(P_SelectionObserver o);
-    public void unregister(P_SelectionObserver o);
-    public void notifyObserver();
-
-}
--- src/unsw/gloriaromanus/SelectionObserver.java
+++
@@ -1,5 +0,0 @@
-package unsw.gloriaromanus;
-
-public interface SelectionObserver {
-    public void update(String p_name);
-}
