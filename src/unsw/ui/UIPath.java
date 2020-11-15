309e3aeb5163cc9c3c277e1674c7f5128dd88254
Anran <z5183829@ad.unsw.edu.au>
2020-11-15 15:46:20 +1100

    pathpath

--- src/unsw/ui/UIPath.java
+++ src/unsw/ui/UIPath.java
@@ -5,7 +5,7 @@ public enum UIPath {
     NEWGAME("src/unsw/ui/GameSetting/GameSetting.fxml"),
     GAME("src/unsw/gloriaromanus/main.fxml"),
     LOADSAVE("src/unsw/ui/LoadSave/LoadSave.fxml"),
-    SAVES("saves/"),
+    SAVES("src/unsw/saves/SavedGames/"),
     TOPBAR("src/unsw/ui/topbar/TopBar.fxml"),
     VIC("src/unsw/ui/VicUI/VicUI.fxml");
 
